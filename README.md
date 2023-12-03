1e7e94ea710e56bc807fa62d6dd1857fef5e7813
pavukane <nguyenpham121998@gmail.com>
2023-12-03 02:58:51 -0500

    update README to include dev setup, add sql script to populate mock data

--- README.md
+++ README.md
@@ -11,3 +11,12 @@
 `uvicorn api.main:app --reload`
 ### Test API by built-in docs:
 [http://127.0.0.1:8000/docs](http://127.0.0.1:8000/docs)
+
+### Development Setup
+- Create new connection in MySQL WorkBench:
+  - Connection name & root password must be same as ```api/dependencies/config.py``` so fastapi can access and interact with local MySQL database in your machine (if you have a different password, change the password in config.py to match yours)
+  - Test connection to make sure MySQL in your machine is running
+- Create a virtualenv and installed all packages above
+- Run the server in venv. MySQL database tables should be populated
+- run SQL commands in ```mysql_scripts/``` to populate crusts and toppings data
+
---
+++ api/mysql_scripts/crust_toppings_populate.sql
@@ -0,0 +1,17 @@
+use pizza_restaurant_api;
+
+-- INSERT INTO toppings (name, price) VALUES ('Olives', 1.80);
+-- INSERT INTO toppings (name, price) VALUES ('Sausage', 2.25);
+-- INSERT INTO toppings (name, price) VALUES ('Bacon', 2.75);
+-- INSERT INTO toppings (name, price) VALUES ('Spinach', 1.90);
+-- INSERT INTO toppings (name, price) VALUES ('Tomatoes', 1.60);
+
+-- select * from toppings;
+
+-- INSERT INTO crusts (name, price) VALUES ('Thin Crust', 3.50);
+-- INSERT INTO crusts (name, price) VALUES ('Thick Crust', 4.00);
+-- INSERT INTO crusts (name, price) VALUES ('Stuffed Crust', 5.00);
+-- INSERT INTO crusts (name, price) VALUES ('Gluten-Free Crust', 4.50);
+-- INSERT INTO crusts (name, price) VALUES ('Cauliflower Crust', 5.50);
+
+select * from crusts;
